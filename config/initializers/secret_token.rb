530b0f2e0c58b899a4658b7bcf6c49f72f91fc68
Aref Aslani <arefaslani@gmail.com>
2014-10-21 12:39:59 +0330

    devex birth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devex2::Application.config.secret_key_base = '7024b08e7ef03411c8073997679abf6892f88674f4c42bd1e1fd606f190f8875d1fb6248119e5706a35f1f89fc4cdcede98b7d5baec98425bf77cdbba17ac376'
