e0ad4c486df1522ef9b5e56dcde2d9a4ce99b270
Aref Aslani <arefaslani@gmail.com>
2014-10-21 16:51:01 +0330

    add some simple tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it "responds to email" do
+    expect(user).to respond_to :email
+  end
+
+  it "responds to password" do
+    expect(user).to respond_to :password
+  end
+
+  it "responds to password_confirmation" do
+    expect(user).to respond_to :password_confirmation
+  end
+
+  it "has a valid factory" do
+    expect(user).to be_valid
+  end
 end
