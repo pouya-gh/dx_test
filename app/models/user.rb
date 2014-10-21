9b35082796c380728270aef8f77c0bf89666245e
Aref Aslani <arefaslani@gmail.com>
2014-10-21 18:46:32 +0330

    add some password tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, length: { minimum: 4, maximum: 30 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,33 @@ describe User do
   it "has a valid factory" do
     expect(user).to be_valid
   end
+
+  describe "password" do
+    it "has a length greater than 4" do
+      user = build(:user, password: '123', password_confirmation: '123')
+      expect(user).to be_invalid
+    end
+
+    it "is less than 30 characters" do
+      user = build(:user, password: "a" * 31, password_confirmation: "a" * 31)
+      expect(user).to be_invalid
+    end
+
+    it "is equal to password_confirmation" do
+      user = build(:user, password: 'test', password_confirmation: 'test2')
+      expect(user).to be_invalid
+    end
+
+    it "is required" do
+      user = build(:user, password: "")
+      expect(user).to be_invalid
+    end
+  end
+
+  describe "password_confirmation" do
+    it "is required" do
+      user = build(:user, password_confirmation: "")
+      expect(user).to be_invalid
+    end
+  end
 end
