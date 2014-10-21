d33db025b8e4b72c5e34aff0d9caa9d11b123e63
Aref Aslani <arefaslani@gmail.com>
2014-10-21 20:23:38 +0330

    add UsersController#new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe "GET #new" do
+    it "assigns a new user to @user" do
+      get :new
+      expect(assigns(:user)).to be_a_new User
+    end
 
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
 end
