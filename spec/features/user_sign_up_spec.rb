3a47dfd266b1a9afd8127320a9bbdf60da35dbb9
Aref Aslani <arefaslani@gmail.com>
2014-10-22 12:26:18 +0330

    refactor user sign up feature spec

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -4,39 +4,34 @@ feature "user sign up" do
   include ApplicationHelper
   let(:user) { build(:user) }
 
-  context 'with valid data' do
-    before(:each) do
-      sign_up(user)
-    end
-
-    scenario "will show up a success alarm" do
-      expect(page).to have_content(I18n.translate('user.register.success'))
-    end
-
-    scenario "render user profile" do
-      expect(page).to have_title(full_title(I18n.translate('user.profile')))
-    end
+  scenario "signing up to the website" do
+    as_a_guest_user
+    given_i_am_in_sign_up_page
+    when_i_submit_the_form_with_valid_data
+    then_i_should_be_in_my_profile_page
+    and_see_a_success_alarm
   end
 
-  context 'with invalid data' do
-    before(:each) do
-      sign_up(build(:user, email: 'test@test'))
-    end
-
-    scenario "will show up a failure alarm" do
-      expect(page).to have_content(I18n.translate('user.register.fail'))
-    end
-
-    scenario "render sign up form" do
-      expect(page).to have_title(full_title(I18n.translate('user.register.text')))
-    end
+  def as_a_guest_user
+    @user = build(:user)
   end
 
-  def sign_up(user)
+  def given_i_am_in_sign_up_page
     visit new_user_path
+  end
+
+  def when_i_submit_the_form_with_valid_data
     fill_in I18n.translate('user.email'), with: user.email
     fill_in I18n.translate('user.password'), with: user.password
     fill_in I18n.translate('user.password_confirmation'), with: user.password_confirmation
     click_button I18n.translate('user.register.text')
+  end
+
+  def then_i_should_be_in_my_profile_page
+    expect(page).to have_title(full_title(I18n.translate('user.profile')))
+  end
+
+  def and_see_a_success_alarm
+    expect(page).to have_content(I18n.translate('user.register.success'))
   end
 end
