128baf60e1fe2dfb1dbf20b91628e61d6ccf7e36
Aref Aslani <arefaslani@gmail.com>
2014-10-22 12:27:59 +0330

    edit and test full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def full_title(title)
-    I18n.translate("devex.title") + " | #{title}"
+  def full_title(title=nil)
+    if title
+      I18n.translate("devex.title") + " | #{title}"
+    else
+      I18n.translate("devex.title")
+    end
   end
 end
