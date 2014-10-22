6ff856345d1ea281d17f8923168e649c4278394b
Aref Aslani <arefaslani@gmail.com>
2014-10-22 14:39:51 +0330

    add sessions#create spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
 
 describe SessionsController do
-  describe "#new" do
+  describe "GET #new" do
     it "renders new template" do
       get :new
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST #create" do
+    before do
+      @user = create(:user)
+    end
+
+    it "creates a cookie with the content of auth_token filed of user" do
+      post :create, session: attributes_for(:user).slice(:email, :password)
+      expect(response.cookies[:auth_token]).to eql(@user.auth_token)
+    end
+
+    it "redirects the user to its profile page" do
+      expect(response).to redirect_to(@user)
+    end
+
+    it "sets the current_user to @user" do
+      expect(controller.current_user).to eql(@user)
     end
   end
 end
