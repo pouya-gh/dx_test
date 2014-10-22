23e4f0c9a6271f7257286cf49bfa90b33dc29453
Aref Aslani <arefaslani@gmail.com>
2014-10-22 14:44:07 +0330

    add sessions#create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,21 @@ class SessionsController < ApplicationController
   def new
       
   end
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+    if @user && @user.authenticate(session_params[:password])
+      sign_in @user
+      redirect_to @user
+    else
+      flash.now[:danger] = t('user.login.fail')
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
