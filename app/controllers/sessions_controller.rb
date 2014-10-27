1325997683fce5aa0353cf642a72b8b33a0aaf13
Aref Aslani <arefaslani@gmail.com>
2014-10-27 22:39:51 +0330

    redirect admin users to their dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,11 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: session_params[:email])
     if @user && @user.authenticate(session_params[:password])
       sign_in @user
-      redirect_to @user
+      if @user.admin?
+        redirect_to [:admin, @user]
+      else
+        redirect_to @user
+      end
     else
       flash.now[:danger] = t('user.login.fail')
       render :new
