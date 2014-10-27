f29659cf91f8e0f6014e942262dcd40fe6fcd76a
Aref Aslani <arefaslani@gmail.com>
2014-10-27 21:42:17 +0330

    add not signed in content to admin users controller spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -3,22 +3,35 @@ require 'spec_helper'
 describe Admin::UsersController do
   include SessionsHelper
   describe "GET #show" do
-    before do
-      # signup the user
-      @user = create(:user, id: 2)
+    context 'signed in' do
+      before do
+        # signup the user
+        @user = create(:user, id: 2)
 
-      # sign in the user
-      sign_in @user
+        # sign in the user
+        sign_in @user
 
-      get :show, id: 2
+        get :show, id: 2
+      end
+
+      it "renders show template" do
+        expect(response).to render_template(:show)
+      end
+
+      it "create an admin instance of the User class" do
+        expect(assigns(:user)).to eq @user
+      end
     end
 
-    it "renders show template" do
-      expect(response).to render_template(:show)
-    end
+    context 'not signed in' do
+      before do 
+        create(:user)
+        get :show, id: 1
+      end
 
-    it "create an admin instance of the User class" do
-      expect(assigns(:user)).to eq @user
+      it "redirects to root path" do
+        expect(response).to redirect_to login_path(redirect_url: request.original_url)
+      end
     end
   end
 end
