11aa9ecd26ef793a27329572c815242c1efbada8
Aref Aslani <arefaslani@gmail.com>
2014-10-29 18:18:24 +0330

    create post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,10 @@
 
 FactoryGirl.define do
   factory :post do
-    digest "MyString"
-    title "MyString"
+    digest "I am a new post"
+    title "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+           vitae quibusdam voluptas incidunt maiores autem unde tenetur maxime,
+           commodi, mollitia atque error officia asperiores eligendi deserunt
+           repellat ex veniam quaerat."
   end
 end
