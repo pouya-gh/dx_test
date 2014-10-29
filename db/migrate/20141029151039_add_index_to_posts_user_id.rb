3981b0a8e34c00c54a4c8717a17b7c3c663d8043
Aref Aslani <arefaslani@gmail.com>
2014-10-29 18:41:36 +0330

    add index to posts user_id

---
+++ db/migrate/20141029151039_add_index_to_posts_user_id.rb
@@ -0,0 +1,5 @@
+class AddIndexToPostsUserId < ActiveRecord::Migration
+  def change
+    add_index :posts, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029150638) do
+ActiveRecord::Schema.define(version: 20141029151039) do
 
   create_table "admins", force: true do |t|
     t.string "email"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20141029150638) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email"
