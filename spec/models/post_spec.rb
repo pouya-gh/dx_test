6c3389968c023e47dba9633703984c36754410fa
Aref Aslani <arefaslani@gmail.com>
2014-10-29 18:25:33 +0330

    test post properties

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) { build(:post) }
+
+  it "has a valid factory" do
+    expect(post).to be_valid
+  end
+
+  it "responds to digest" do
+    expect(post).to respond_to :digest
+  end
+
+  it "responds to title" do
+    expect(post).to respond_to :title
+  end
 end
