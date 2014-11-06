66d73b9848a6622a7306dba149603a0cb5e5e38e
Aref Aslani <arefaslani@gmail.com>
2014-11-06 09:57:13 +0330

    test posts controller new action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe PostsController do
-  
+  describe "GET #new" do
+    before do
+      get :new
+    end
+
+    it "renders new template" do
+      expect(response).to render_template(:new)
+    end
+
+    it "assings a new post" do
+      expect(assigns(:post)).to be_a_new(Post)
+    end
+  end
 end
