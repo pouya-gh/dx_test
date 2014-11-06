683d094abde22d37f129dc4c16dfde1887f3e1ba
Aref Aslani <arefaslani@gmail.com>
2014-11-06 10:07:26 +0330

    refactor posts controller spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,17 +1,32 @@
 require 'spec_helper'
 
 describe PostsController do
+  include SessionsHelper
   describe "GET #new" do
-    before do
-      get :new
+    context 'signed in' do
+      before do
+        user = create(:user)
+        sign_in user
+        get :new
+      end
+
+      it "renders new template" do
+        expect(response).to render_template(:new)
+      end
+
+      it "assings a new post" do
+        expect(assigns(:post)).to be_a_new(Post)
+      end
     end
 
-    it "renders new template" do
-      expect(response).to render_template(:new)
-    end
+    context 'not signed in' do
+      before do
+        get :new
+      end
 
-    it "assings a new post" do
-      expect(assigns(:post)).to be_a_new(Post)
+      it "redirects the user to login path" do
+        expect(response).to redirect_to login_path(redirect_url: request.original_url)
+      end
     end
   end
 end
