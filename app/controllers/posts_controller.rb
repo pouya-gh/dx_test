65a2a123abbc16597a0bada8f71a8d137f6944c7
Aref Aslani <arefaslani@gmail.com>
2014-11-06 11:18:57 +0330

    define posts#create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def create
+    @post = current_user.posts.new(post_params)
+    begin
+      @post.save!
+      flash[:success] = t('post.create.success')
+      redirect_to admin_user_path(current_user)
+    rescue
+      render :new
+      flash.now[:danger] = t('post.create.failure')
+    end
+  end
+
   private
 
   def check_signed_in
@@ -18,5 +30,9 @@ class PostsController < ApplicationController
       flash[:warning] = t('authorization.errors.signin')
       redirect_to login_path(redirect_url: request.original_url)
     end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :digest)
   end
 end  
