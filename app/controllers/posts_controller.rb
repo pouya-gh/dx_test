8febf9345e4979062dba6d8099a6f31c9f8c707d
Aref Aslani <arefaslani@gmail.com>
2014-11-06 10:12:12 +0330

    add posts controller new action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,16 @@
 class PostsController < ApplicationController
-  
+  before_filter :check_signed_in
+
+  def new
+    @post = Post.new 
+  end
+
+  private
+
+  def check_signed_in
+    unless signed_in?
+      flash[:warning] = t('authorization.errors.signin')
+      redirect_to login_path(redirect_url: request.original_url)
+    end
+  end
 end  
