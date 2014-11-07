36a95dcfc760915a887b395913b05af392406b55
Aref Aslani <arefaslani@gmail.com>
2014-11-07 13:28:25 +0330

    refactor managing posts steps

--- spec/features/steps/managing_posts_steps_macro.rb
+++ spec/features/steps/managing_posts_steps_macro.rb
@@ -1,13 +1,5 @@
 module ManagingPostsStepsMacro
   # writing posts
-  def given_i_am_a_admin_user
-    @user = create(:admin)
-  end
-
-  def and_i_am_signed_in
-    sign_in @user
-  end
-
   def when_i_go_to_new_post_path
     visit new_post_path
   end
@@ -17,14 +9,11 @@ module ManagingPostsStepsMacro
     click_button I18n.translate('post.create.text')
   end
 
-  def then_i_shoud_be_in_my_dashboard
-    expect(page).to have_title(full_title(I18n.translate('user.admin.dashboard')))
-  end
-
   def and_see_post_created_message
     expect(page).to have_content(I18n.translate('post.create.success'))
   end
 
+  # editing posts
   def when_i_go_to_edit_post_path
     @post = create(:post)
     visit edit_post_path(@post)
@@ -37,6 +26,19 @@ module ManagingPostsStepsMacro
 
   def and_see_post_edited_message
     expect(page).to have_content(I18n.translate('post.edit.success'))
+  end
+
+  # shared
+  def given_i_am_a_admin_user
+    @user = create(:admin)
+  end
+
+  def and_i_am_signed_in
+    sign_in @user
+  end
+
+  def then_i_shoud_be_in_my_dashboard
+    expect(page).to have_title(full_title(I18n.translate('user.admin.dashboard')))
   end
 
   private
