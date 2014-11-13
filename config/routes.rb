f0957749fc44f045705cf15de58f5a3eaefb43c0
Aref Aslani <arefaslani@gmail.com>
2014-11-13 22:25:49 +0330

    add posts index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Devex2::Application.routes.draw do
   
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create]
+  resources :posts, only: [:index]
 
   namespace :admin do
     resources :users, only: [:show]
