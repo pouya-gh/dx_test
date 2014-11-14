362e614a10b36f2f2adfbde351a57560246617f1
Aref Aslani <arefaslani@gmail.com>
2014-11-14 14:15:33 +0330

    add medium and small size of images

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -32,9 +32,13 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :small do
+    process :resize_to_fit => [75, 50]
+  end
+
+  version :medium do
+    process :resize_to_fit => [200, 125]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
