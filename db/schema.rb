84b6eb7d65cabd466c4952aff1aaa1d6ed08a26e
Aref Aslani <arefaslani@gmail.com>
2014-11-14 13:33:27 +0330

    add image filed to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111150055) do
+ActiveRecord::Schema.define(version: 20141114100224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141111150055) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "tags",       default: [], array: true
+    t.string   "image"
   end
 
   add_index "posts", ["tags"], name: "index_posts_on_tags", using: :gin
