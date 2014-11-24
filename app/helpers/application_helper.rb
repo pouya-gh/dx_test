34bbe2d54bc2e62662061f801eb5de2026ecb756
Aref Aslani <arefaslani@gmail.com>
2014-11-25 02:06:17 +0330

    add metadata_for helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,28 @@ module ApplicationHelper
       I18n.translate("devex.title")
     end
   end
+
+  def metadata_for(video_path, *options)
+    video = FFMPEG::Movie.new(video_path)
+    duration = Time.at(video.duration).utc.strftime("%H:%M:%S")
+    size = number_to_human_size(video.size)
+    metadata = []
+
+    if options.empty?
+      metadata << "duration: #{duration}"
+      metadata << "size: #{size}"
+      metadata.join(" | ")
+    else
+      options.each do |option|
+        if option.to_s == "size"
+          metadata << "size: #{size}"
+        elsif option.to_s == "duration"
+          metadata << "duration: #{duration}"
+        else
+          metadata << "#{option.to_s}: #{video.send(option.to_sym)}"
+        end
+      end
+      metadata.join(" | ")
+    end
+  end
 end
