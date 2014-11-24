511bce2e16ce53fd87aad7b035942543b1926f5b
Aref Aslani <arefaslani@gmail.com>
2014-11-24 23:35:52 +0330

    mount video uploader in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,11 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :image, PostImageUploader
+  mount_uploader :video, PostVideoUploader
 
   validates :title, presence: true
   validates :digest, presence: true
+  validates :video, presence: true
 
   def self.find_by_tag(tag)
     Post.where("'#{tag}' = ANY (tags)")
