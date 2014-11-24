264554cc009825736d6ec96e95dabb0059cc0ee9
Aref Aslani <arefaslani@gmail.com>
2014-11-25 02:43:37 +0330

    change uploaded images directory

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -9,7 +9,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
   storage :file
   
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "assets/images/posts/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
